3ecce1d5a81be8a43cf226791ac4e2b485f351ee
Phousanak Han <phoukao200@gmail.com>
2020-06-30 15:19:12 -0600

    working on device. Fail on simulator

--- WeatherWear/CurrentWeatherViewModel.swift
+++ WeatherWear/CurrentWeatherViewModel.swift
@@ -10,33 +10,44 @@ import SwiftUI
 import Combine
 import UIKit
 import MapKit
+import CoreLocation
 
 final class CurrentWeatherViewModel: ObservableObject {
     @Published var current: Weather?
+    @ObservedObject var locationManager = LocationManager()
+
     
     init() {
-    /*var locationManager = CLLocationManager()
-    locationManager.requestWhenInUseAuthorization()
+    var locationManager1 = CLLocationManager()
+    locationManager1.requestWhenInUseAuthorization()
     var CurLoc: CLLocation!
-    if( CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
-        CLLocationManager.authorizationStatus() ==  .authorizedAlways){
-        CurLoc = locationManager.location
+    if( CLLocationManager.authorizationStatus() == .authorizedWhenInUse){
+        CurLoc = locationManager1.location
     }
         let long = CurLoc.coordinate.longitude
         let lat = CurLoc.coordinate.latitude
+        print(long)
+        print(lat)
+
         
         let geoCoder = CLGeocoder()
         let location = CLLocation(latitude: lat, longitude: long)
-        geoCoder.reverseGeocodeLocation(location, completionHandler: {
-            placemarks, error -> Void in
-            
-            guard let placeMark = placemarks?.first else {return}
-            if let locationName = placeMark.location {
-                print(locationName)
-            }
-        } )*/
-        
-        
+        geoCoder.reverseGeocodeLocation(location,
+                 completionHandler: { (placemarks, error) -> Void in
+                    if error != nil {
+                        fatalError()
+                    }
+                    
+                    let pm = placemarks! as [CLPlacemark]
+                    if (placemarks?.count)! > 0 {
+                        let pm = placemarks![0]
+                        //print(pm.locality!)
+                        self.fetch(city: pm.locality!)
+                    }
+                    else{
+                        fatalError()
+                    }
+            } )
         self.fetch()
     }
 }
@@ -44,7 +55,7 @@ final class CurrentWeatherViewModel: ObservableObject {
 extension CurrentWeatherViewModel {
     
     
-    func fetch( city: String = "edmonton"){
+    func fetch( city: String = "london"){
         
         API.fetchCurrentWeather(by: city) {
             self.current = $0
